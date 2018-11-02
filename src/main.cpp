931c7f267103ed2861140fd7c2cd796a54920f7b
u-amine <amine.uenal@gmail.com>
2018-11-02 07:58:32 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <libsnark/common/default_types/r1cs_gg_ppzksnark_pp.hpp>
 #include <libsnark/relations/constraint_satisfaction_problems/r1cs/examples/r1cs_examples.hpp>
 #include <libsnark/zk_proof_systems/ppzksnark/r1cs_gg_ppzksnark/r1cs_gg_ppzksnark.hpp>
+#include <fstream>
 
 using namespace libsnark;
 
@@ -18,18 +19,63 @@ using namespace libsnark;
  *     a primary input for CS, and a proof.
  */
 template<typename ppT>
-bool run_r1cs_gg_ppzksnark(const r1cs_example<libff::Fr<ppT> > &example)
+void run_r1cs_gg_ppzksnark(const r1cs_example<libff::Fr<ppT> > &example)
 {
     libff::print_header("R1CS GG-ppzkSNARK Generator");
     r1cs_gg_ppzksnark_keypair<ppT> keypair = r1cs_gg_ppzksnark_generator<ppT>(example.constraint_system);
     printf("\n"); libff::print_indent(); libff::print_mem("after generator");
+    
+    std::ofstream pfile ("pfile");
+    std::ofstream vfile ("vfile");
+    pfile << keypair.pk;
+    vfile << keypair.vk;
+}
+
+
+template<typename ppT>
+void run_prover(const r1cs_example<libff::Fr<ppT> > &example) {
+    r1cs_gg_ppzksnark_proving_key<ppT> pk;
+    std::ifstream pfile ("pfile");
+    pfile >> pk;
+    
+    libff::print_header("R1CS GG-ppzkSNARK Prover");
+    r1cs_gg_ppzksnark_proof<ppT> proof = r1cs_gg_ppzksnark_prover<ppT>(pk, example.primary_input, example.auxiliary_input);
+    printf("\n"); libff::print_indent(); libff::print_mem("after prover");
+    
+    std::ofstream proofile ("proof");
+    proofile << proof;
+}
+
+template<typename ppT>
+bool run_verifier(const r1cs_example<libff::Fr<ppT> > &example) {
+    r1cs_gg_ppzksnark_verification_key<ppT> vk;
+    r1cs_gg_ppzksnark_proof<ppT> proof;
+    std::ifstream vfile ("vfile");
+    vfile >> vk;
+    std::ifstream proofile ("proof");
+    proofile >> proof;
+    
+    libff::print_header("Preprocess verification key");
+    r1cs_gg_ppzksnark_processed_verification_key<ppT> pvk = r1cs_gg_ppzksnark_verifier_process_vk<ppT>(vk);
+    
+    libff::print_header("R1CS GG-ppzkSNARK Verifier");
+    const bool ans = r1cs_gg_ppzksnark_verifier_strong_IC<ppT>(vk, example.primary_input, proof);
+    printf("\n"); libff::print_indent(); libff::print_mem("after verifier");
+    printf("* The verification result is: %s\n", (ans ? "PASS" : "FAIL"));
+
+    libff::print_header("R1CS GG-ppzkSNARK Online Verifier");
+    const bool ans2 = r1cs_gg_ppzksnark_online_verifier_strong_IC<ppT>(pvk, example.primary_input, proof);
+    assert(ans == ans2);
+
+    return ans;
+}
+
+/*
 
     libff::print_header("Preprocess verification key");
     r1cs_gg_ppzksnark_processed_verification_key<ppT> pvk = r1cs_gg_ppzksnark_verifier_process_vk<ppT>(keypair.vk);
 
-    libff::print_header("R1CS GG-ppzkSNARK Prover");
-    r1cs_gg_ppzksnark_proof<ppT> proof = r1cs_gg_ppzksnark_prover<ppT>(keypair.pk, example.primary_input, example.auxiliary_input);
-    printf("\n"); libff::print_indent(); libff::print_mem("after prover");
+    
 
     libff::print_header("R1CS GG-ppzkSNARK Verifier");
     const bool ans = r1cs_gg_ppzksnark_verifier_strong_IC<ppT>(keypair.vk, example.primary_input, proof);
@@ -41,13 +87,15 @@ bool run_r1cs_gg_ppzksnark(const r1cs_example<libff::Fr<ppT> > &example)
     assert(ans == ans2);
 
     return ans;
-}
+}*/
 
 template<typename ppT>
 void test_r1cs_gg_ppzksnark(size_t num_constraints, size_t input_size)
 {
     r1cs_example<libff::Fr<ppT> > example = generate_r1cs_example_with_binary_input<libff::Fr<ppT> >(num_constraints, input_size);
-    const bool bit = run_r1cs_gg_ppzksnark<ppT>(example);
+    run_r1cs_gg_ppzksnark<ppT>(example);
+    run_prover<ppT>(example);
+    const bool bit = run_verifier<ppT>(example);
     assert(bit);
 }
 
